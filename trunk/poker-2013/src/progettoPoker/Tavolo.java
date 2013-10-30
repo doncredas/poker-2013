c6b2ed3d0e5b23c3bc2d4bbd5338b0409d214646
Giovv92 <Giovv92@12f011b8-40de-ed8b-c6d1-a6307bff6ef0>
2013-10-30 18:04:00 +0000

    Prima istanza delle classi

---
+++ trunk/poker-2013/src/progettoPoker/Tavolo.java
@@ -0,0 +1,12 @@
+package progettoPoker;
+
+public class Tavolo {
+	Giocatore[]g;
+	Dealer d;
+	Carta []mazzo=new Carta[52];
+	int carteComuni;
+	public Tavolo() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
