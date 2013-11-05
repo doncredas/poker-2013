ec567e40ae188a75903d87b6228002c7b970707a
Giovv92 <Giovv92@12f011b8-40de-ed8b-c6d1-a6307bff6ef0>
2013-11-05 15:09:01 +0000

    Aggiunti commenti

--- poker-2013/src/progettoPoker/Dealer.java
+++ poker-2013/src/progettoPoker/Dealer.java
@@ -1,7 +1,20 @@
 package progettoPoker;
-
+/**
+ * Classe che gestisce i bui e il mazzo: rialza i bui dopo un certo numero di mani, preleva le quote dai giocatori,
+ * mischia il mazzo, da le carte ecc..
+ * 
+ *
+ */
 public class Dealer {
-	int buioPosizione,nMano,grandeBuio,piccoloBuio;
+	private int buioPosizione;//indica il giocatore che dovrà pagare il piccolo buio
+	private int nMano;//a che mano siamo
+	private int piccoloBuio;//il valore del piccolo buio (il grande buio è il doppio)
+	private Carta []mazzo=new Carta[52];//il mazzo visto come array di 52 carte
+	private int primaCarta;/*invece di eliminare le carte che diamo ai giocatori dal mazzo, manteniamo un indice 
+	*della prima "carta utile" del mazzo
+	*/
+	private Giocatore[]g;
+	private Carta[] carteComuni=new Carta[5];
 	public Dealer() {
 		// TODO Auto-generated constructor stub
 	}
