f263388f2489b9e66126417805ea8b821922666b
Giovv92 <Giovv92@12f011b8-40de-ed8b-c6d1-a6307bff6ef0>
2013-11-12 09:43:35 +0000

    Aggiunta packege net

---
+++ poker-2013/src/net/EchoClient.java
@@ -0,0 +1,38 @@
+package net;
+
+import java.io.*;
+import java.net.*;
+
+public class EchoClient {
+      public static void main(String[] args) throws IOException {
+
+        Socket echoSocket = null;
+        PrintWriter out = null;
+        BufferedReader in = null;
+        String nomeSocket="127.0.0.1";
+
+        try {
+            echoSocket = new Socket(nomeSocket, 7777);
+            out = new PrintWriter(echoSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host: "+nomeSocket);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to: "+args[0]);
+            System.exit(1);
+        }
+
+        System.out.println("Server welcome is: "+in.readLine());
+
+        out.println("Hello how are you");
+        System.out.println("Server responded: "+in.readLine());
+
+        out.println("Some other stuff");
+        System.out.println("Server now responded: "+in.readLine());
+
+        out.close();
+        in.close();
+        echoSocket.close();
+    }
+}
