e387094f293b0396c57320ceb023e4b15cb80137
pepistiko@gmail.com <pepistiko@gmail.com@12f011b8-40de-ed8b-c6d1-a6307bff6ef0>
2013-12-05 15:57:19 +0000

    new graphics

--- trunk/poker-2013/src/grafica/FinestraPunteggio.java
+++ trunk/poker-2013/src/grafica/FinestraPunteggio.java
@@ -1,64 +1,82 @@
 package grafica;
                          //AL CLICK DEL BOTTONE PUNTI APRE UNA FINESTRA CON LA LISTA DEI PUNTEGGI
+import java.awt.Container;
+import java.awt.Image;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.io.File;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-public class FinestraPunteggio implements MouseListener{
+public class FinestraPunteggio extends JFrame implements MouseListener
+{
+	static File Immagini=new File("Immagini");
+	File Bottoni=new File(Immagini.getAbsolutePath()+"\\Bottoni");
 	
-	JFrame punti=new JFrame("Punteggio");
+	public FinestraPunteggio()
+	{
+	      super("Punteggio");
+	}
 	@Override
-	public void mouseClicked(MouseEvent e) {
+	public void mouseClicked(MouseEvent e)
+	{
 		// TODO Auto-generated method stub
-       if(punti.isActive())
+
+		if(this.isActive())
        {
     	   
-    	   punti.setDefaultCloseOperation(punti.DISPOSE_ON_CLOSE);
+    	   this.setDefaultCloseOperation(this.DISPOSE_ON_CLOSE);
        }
 	}
 
 	@Override
-	public void mouseEntered(MouseEvent e) {
+	public void mouseEntered(MouseEvent e)
+    {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) 
+	{
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
+	public void mousePressed(MouseEvent e)
+	{
+
 	}
 
 	@Override
-	public void mousePressed(MouseEvent e) {
+	public void mouseReleased(MouseEvent e) 
+	{
 		// TODO Auto-generated method stub
-		
-		
-	}
 
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-		if(punti.isVisible())
+		
+		if(this.isVisible())
 	       {
 	    	   
-	    	   punti.setVisible(false);
+	    	   this.setVisible(false);
 	       }
 		else{
-		
-		ImageIcon sfondo = new ImageIcon("C:\\poo_eclipse\\immagini\\punteggio.jpg");
-		
-		punti.setResizable(false);
-		punti.setBounds(115, 15, 600, 650);
-		punti.setContentPane(new JLabel(sfondo));
-		punti.setSize(600,665);
-	    punti.setSize(600,666);
+			
 	    
-	    punti.setVisible(true);
+	    ImageIcon sfondo = new ImageIcon(Bottoni.getAbsolutePath()+"\\punteggio.jpg");
+		this.setResizable(false);
+		this.setBounds(115, 15, 600, 650);
+		this.setContentPane(new JLabel(sfondo));
+		this.setSize(600,665);
+	    this.setSize(600,666);
+	    
+	    this.setVisible(true);
 		}
 	}
 
 }
+
+
+
